adf2ae795ac75624769da0214c393e17e4bdb145
KAVIYA <KAVIYA@KAVIYA-RAMADOSS27>
2024-09-04 14:28:42 +0530

    code

---
+++ C2TC_Kaviya/src/com/TNS/day2/ThirdProgram.java
@@ -0,0 +1,24 @@
+package com.TNS.day2;
+
+public class ThirdProgram {
+	public static void main(String args[])
+	{
+		//looping
+		for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+		//while loop
+		int i = 1;
+        while (i <= 5) {
+            System.out.println("numbers: " + i);
+            i++;
+        }
+        //do while
+        int j = 1;
+        do {
+            System.out.println("Integer: " + j);
+            j++;
+        } while (j <= 5);
+	}
+
+}
